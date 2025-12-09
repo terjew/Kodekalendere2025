dbbc712464480e8dfe712d8f2e0b30a703e19917
Terje Wiesener <wiesener@gmail.com>
2025-12-09 15:49:57 +0100

    Day 9 part2

--- AoC/Source/AoC/Private/Day09.cpp
+++ AoC/Source/AoC/Private/Day09.cpp
@@ -1,6 +1,8 @@
 #include "Day09.h"
 #include "Util.h"
 #include "Linq.h"
+#include "GeomTools.h"
+#include "Algo/Reverse.h"
 
 ADay09::ADay09()
 {
@@ -29,7 +31,85 @@ int64 ADay09::SolvePart1()
 
 int64 ADay09::SolvePart2()
 {
-	return 0;
+	TArray<FVector2D> Points = UnrealLinq::Select(CornerPoints, [](const FIntPoint& P) {
+		return FVector2D(static_cast<double>(P.X), static_cast<double>(P.Y));
+	});
+
+	TArray<IntLineSegment> Edges;
+	for (int i = 0; i < CornerPoints.Num(); i++)
+	{
+		FIntPoint Start = CornerPoints[i];
+		FIntPoint End = CornerPoints[(i + 1) % CornerPoints.Num()];
+		Edges.Add(IntLineSegment(Start, End));
+	}
+
+	int64 bestArea = 0;
+	for (int i = 0; i < CornerPoints.Num(); i++)
+	{
+		FIntVector2 PointA = CornerPoints[i];
+		for (int j = i + 1; j < CornerPoints.Num(); j++)
+		{
+			FIntVector2 PointB = CornerPoints[j];
+			int x0 = FMath::Min(PointA.X, PointB.X);
+			int x1 = FMath::Max(PointA.X, PointB.X);
+			int y0 = FMath::Min(PointA.Y, PointB.Y);
+			int y1 = FMath::Max(PointA.Y, PointB.Y);
+			
+			int64 dx = x1 - x0 + 1LL;
+			int64 dy = y1 - y0 + 1LL;
+			int64 area = dx * dy;
+			if (area > bestArea)
+			{
+				FIntPoint ul = FIntPoint(x0, y0);
+				FIntPoint ur = FIntPoint(x1, y0);
+				FIntPoint ll = FIntPoint(x0, y1);
+				FIntPoint lr = FIntPoint(x1, y1);
+
+				TArray<FIntPoint> RectPoints = { ul, ur, lr, ll };
+				bool allInside = true;
+				for (const auto& RectPoint : RectPoints)
+				{
+					if (!FGeomTools2D::IsPointInPolygon(FVector2D(static_cast<double>(RectPoint.X), static_cast<double>(RectPoint.Y)), Points)) {
+						allInside = false;
+						break;
+					}
+				}
+				if (!allInside)
+				{
+					continue;
+				}
+
+				TArray<IntLineSegment> RectEdges = {
+					IntLineSegment(ul, ur),
+					IntLineSegment(ur, lr),
+					IntLineSegment(lr, ll),
+					IntLineSegment(ll, ul)
+				};
+				bool bIntersects = false;
+				for (const auto& RectEdge : RectEdges)
+				{
+					for (const auto& Edge : Edges)
+					{
+						if (RectEdge.Intersects(Edge))
+						{
+							bIntersects = true;
+							break;
+						}
+					}
+					if (bIntersects)
+					{
+						break;
+					}
+				}
+				if (bIntersects)
+				{
+					continue;
+				}
+				bestArea = area;
+			}
+		}
+	}
+	return bestArea;
 }
 
 void ADay09::BeginPlay()
@@ -39,8 +119,9 @@ void ADay09::BeginPlay()
 	CornerPoints = UnrealLinq::Select(Lines, [](const FString& Line) {
 		FString Left, Right;
 		Line.Split(TEXT(","), &Left, &Right);
-		return FIntVector2(FCString::Atoi(*Left), FCString::Atoi(*Right));
+		return FIntPoint(FCString::Atoi(*Left), FCString::Atoi(*Right));
 	});
 
 	Part1 = SolvePart1();
+	Part2 = SolvePart2();
 }
--- AoC/Source/AoC/Public/Day09.h
+++ AoC/Source/AoC/Public/Day09.h
@@ -3,6 +3,39 @@
 #include "DayBase.h"
 #include "Day09.generated.h"
 
+struct IntLineSegment
+{
+	FIntPoint Start;
+	FIntPoint End;
+	bool bIsVertical;
+	IntLineSegment() : Start(0),End(0),bIsVertical(false) {}
+	IntLineSegment(const FIntPoint& InStart, const FIntPoint& InEnd)
+	{
+		Start = FIntPoint(FMath::Min(InStart.X, InEnd.X), FMath::Min(InStart.Y, InEnd.Y));
+		End = FIntPoint(FMath::Max(InStart.X, InEnd.X), FMath::Max(InStart.Y, InEnd.Y));
+		bIsVertical = (Start.X == End.X);
+	}
+
+	bool Intersects(const IntLineSegment& Other) const
+	{
+		if (Other.bIsVertical == bIsVertical)
+		{
+			return false;
+		}
+		const IntLineSegment& VerticalSeg = bIsVertical ? *this : Other;
+		const IntLineSegment& HorizontalSeg = bIsVertical ? Other : *this;
+		if (VerticalSeg.Start.X <= HorizontalSeg.Start.X || VerticalSeg.Start.X >= HorizontalSeg.End.X)
+		{
+			return false;
+		}
+		if (HorizontalSeg.Start.Y <= VerticalSeg.Start.Y || HorizontalSeg.Start.Y >= VerticalSeg.End.Y)
+		{
+			return false;
+		}
+		return true;
+	}
+};
+
 UCLASS()
 class AOC_API ADay09 : public ADayBase
 {
@@ -22,7 +55,7 @@ protected:
 
 private:
 	TArray<TArray<TCHAR>> Data;
-	TArray<FIntVector2> CornerPoints;
+	TArray<FIntPoint> CornerPoints;
 
 	virtual FString DayName() const override { return "Day09"; }
 	virtual int64 SolvePart1() override;
